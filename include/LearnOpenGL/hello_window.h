f628001a6bde74b1d61cd4c41bb3fbaaf6db5057
Ryan Wicks <rwicks@2grobotics.com>
2019-05-08 13:19:25 -0400

    Working Hello Window tutorial.

---
+++ include/LearnOpenGL/hello_window.h
@@ -0,0 +1,7 @@
+#ifndef HELLO_WINDOW_H
+#define HELLO_WINDOW_H
+
+//https://learnopengl.com/Getting-started/Hello-Window
+int helloWindow();
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Ryan Wicks
+ * @brief Main entry point for tutorial code.
+ * @version 0.1
+ * @date 2019-05-08
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+#include "LearnOpenGL/hello_window.h"
+
+int main ( int argc, char * argv[]){
+
+    return helloWindow();
+
+}
